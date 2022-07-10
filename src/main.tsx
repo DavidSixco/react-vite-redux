ca9c22b1ba2642d8c850e1cd3f8137495e91a5f1
DavidSixco <93099836+DavidSixco@users.noreply.github.com>
2022-07-10 08:29:21 -0600

    tag: configuration issue: relative path (alias path) in vite.config.ts anf tsconfig and add dependecies @types/node

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store/store'
-import Authentication from './configuration/Authentication';
+import { store } from '@store/store'
+import Home from '@pages/home';
+import Authentication from '@configuration/Authentication';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <Authentication>
-        <App />
+        {/* <App /> */}
+        <Home></Home>
       </Authentication>
     </Provider>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    //Configuramos las rutas relativas (alias path) para mayor eficiencia
+    //se necesita @types/node para que __dirname no se queje
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@root': path.resolve(__dirname, './src'),
+      // '@hooks': path.resolve(__dirname,'.hooks'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@configuration': path.resolve(__dirname, './src/configuration'),
+      '@enum': path.resolve(__dirname, './src/enum'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@types': path.resolve(__dirname, './src/types'),
+    }
+  },
   plugins: [react()]
 })
