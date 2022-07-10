a7a3547dbc45d74a7dcbe2af84be07526bf47cd0
DavidSixco <93099836+DavidSixco@users.noreply.github.com>
2022-07-10 09:24:32 -0600

    tag: dependecies issue: add ant design and add relative path to hook and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
+import 'antd/dist/antd.css';
 import { Provider } from 'react-redux';
 import { store } from '@store/store'
 import Home from '@pages/home';
@@ -12,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <Authentication>
         {/* <App /> */}
-        <Home></Home>
+        <Home />
       </Authentication>
     </Provider>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
     //Configuramos las rutas relativas (alias path) para mayor eficiencia
     //se necesita @types/node para que __dirname no se queje
     alias: {
-      '@': path.resolve(__dirname, './src'),
       '@root': path.resolve(__dirname, './src'),
-      // '@hooks': path.resolve(__dirname,'.hooks'),
+      '@layout': path.resolve(__dirname, '.src/layout'),
+      '@hooks': path.resolve(__dirname, '.src/hooks'),
       '@components': path.resolve(__dirname, './src/components'),
       '@configuration': path.resolve(__dirname, './src/configuration'),
       '@enum': path.resolve(__dirname, './src/enum'),
